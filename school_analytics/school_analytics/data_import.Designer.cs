3ea16edd63e5c2ca36648bb186d93a0e813ea4a6
dashaschev <dashaschev@gmail.com>
2025-10-12 00:19:18 +0300

    .

---
+++ school_analytics/school_analytics/data_import.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace school_analytics
+{
+    partial class data_import
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.test_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.theme_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subject_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.class_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user_account_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.test_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.execution_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.theme_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subject_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.class_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.attempt_count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.question_count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.test_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.full_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.creation_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(251)))), ((int)(((byte)(233)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.test_id,
+            this.theme_id,
+            this.subject_id,
+            this.class_id,
+            this.user_account_id,
+            this.test_name,
+            this.execution_time,
+            this.theme_name,
+            this.subject_name,
+            this.class_name,
+            this.attempt_count,
+            this.question_count,
+            this.test_type,
+            this.full_name,
+            this.creation_date});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(488, 267);
+            this.dataGridView1.TabIndex = 80;
+            // 
+            // test_id
+            // 
+            this.test_id.DataPropertyName = "test_id";
+            this.test_id.HeaderText = "test_id";
+            this.test_id.Name = "test_id";
+            this.test_id.ReadOnly = true;
+            this.test_id.Visible = false;
+            // 
+            // theme_id
+            // 
+            this.theme_id.DataPropertyName = "theme_id";
+            this.theme_id.HeaderText = "theme_id";
+            this.theme_id.Name = "theme_id";
+            this.theme_id.ReadOnly = true;
+            this.theme_id.Visible = false;
+            // 
+            // subject_id
+            // 
+            this.subject_id.DataPropertyName = "subject_id";
+            this.subject_id.HeaderText = "subject_id";
+            this.subject_id.Name = "subject_id";
+            this.subject_id.ReadOnly = true;
+            this.subject_id.Visible = false;
+            // 
+            // class_id
+            // 
+            this.class_id.DataPropertyName = "class_id";
+            this.class_id.HeaderText = "class_id";
+            this.class_id.Name = "class_id";
+            this.class_id.ReadOnly = true;
+            this.class_id.Visible = false;
+            // 
+            // user_account_id
+            // 
+            this.user_account_id.DataPropertyName = "user_account_id";
+            this.user_account_id.HeaderText = "user_account_id";
+            this.user_account_id.Name = "user_account_id";
+            this.user_account_id.ReadOnly = true;
+            this.user_account_id.Visible = false;
+            // 
+            // test_name
+            // 
+            this.test_name.DataPropertyName = "test_name";
+            this.test_name.HeaderText = "Тест";
+            this.test_name.Name = "test_name";
+            this.test_name.ReadOnly = true;
+            this.test_name.Width = 150;
+            // 
+            // execution_time
+            // 
+            this.execution_time.DataPropertyName = "execution_time";
+            this.execution_time.HeaderText = "Час";
+            this.execution_time.Name = "execution_time";
+            this.execution_time.ReadOnly = true;
+            this.execution_time.Width = 40;
+            // 
+            // theme_name
+            // 
+            this.theme_name.DataPropertyName = "theme_name";
+            this.theme_name.HeaderText = "Тема";
+            this.theme_name.Name = "theme_name";
+            this.theme_name.ReadOnly = true;
+            // 
+            // subject_name
+            // 
+            this.subject_name.DataPropertyName = "subject_name";
+            this.subject_name.HeaderText = "Предмет";
+            this.subject_name.Name = "subject_name";
+            this.subject_name.ReadOnly = true;
+            // 
+            // class_name
+            // 
+            this.class_name.DataPropertyName = "class_name";
+            this.class_name.HeaderText = "Клас";
+            this.class_name.Name = "class_name";
+            this.class_name.ReadOnly = true;
+            this.class_name.Width = 40;
+            // 
+            // attempt_count
+            // 
+            this.attempt_count.DataPropertyName = "attempt_count";
+            this.attempt_count.HeaderText = "Спроб";
+            this.attempt_count.Name = "attempt_count";
+            this.attempt_count.ReadOnly = true;
+            this.attempt_count.Width = 45;
+            // 
+            // question_count
+            // 
+            this.question_count.DataPropertyName = "question_count";
+            this.question_count.HeaderText = "К-сть питань";
+            this.question_count.Name = "question_count";
+            this.question_count.ReadOnly = true;
+            // 
+            // test_type
+            // 
+            this.test_type.DataPropertyName = "test_type";
+            this.test_type.HeaderText = "Тип тесту";
+            this.test_type.Name = "test_type";
+            this.test_type.ReadOnly = true;
+            // 
+            // full_name
+            // 
+            this.full_name.DataPropertyName = "full_name";
+            this.full_name.HeaderText = "Створив";
+            this.full_name.Name = "full_name";
+            this.full_name.ReadOnly = true;
+            // 
+            // creation_date
+            // 
+            this.creation_date.DataPropertyName = "creation_date";
+            this.creation_date.HeaderText = "Дата створення";
+            this.creation_date.Name = "creation_date";
+            this.creation_date.ReadOnly = true;
+            this.creation_date.Width = 70;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(252)))), ((int)(((byte)(222)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(248)))), ((int)(((byte)(206)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(633, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 40);
+            this.button2.TabIndex = 88;
+            this.button2.Text = "Імпортувати";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(167)))), ((int)(((byte)(252)))), ((int)(((byte)(222)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(248)))), ((int)(((byte)(206)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(378, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 40);
+            this.button1.TabIndex = 89;
+            this.button1.Text = "Додати викладачів";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 90;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 356);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 91;
+            // 
+            // data_import
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(251)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(1151, 453);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "data_import";
+            this.Text = "data_import";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn test_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn theme_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subject_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn class_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user_account_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn test_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn execution_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn theme_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subject_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn class_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn attempt_count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn question_count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn test_type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn full_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn creation_date;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ school_analytics/school_analytics/data_menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace school_analytics
+{
+    public partial class data_menu : Form
+    {
+        public data_menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
